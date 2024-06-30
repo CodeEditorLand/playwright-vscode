fab3b032bffbc61c67a74bfb17f7fad6e2159ff6
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-30 04:12:53 +0300

    squash!

--- Source/extension.ts
+++ Source/extension.ts
@@ -284,7 +284,7 @@ export class Extension implements RunHooks {
         continue;
       }
 
-      const minimumPlaywrightVersion = 1.28;
+      const minimumPlaywrightVersion = 1.38;
       if (playwrightInfo.version < minimumPlaywrightVersion) {
         if (userGesture) {
           this._vscode.window.showWarningMessage(
@@ -482,8 +482,10 @@ export class Extension implements RunHooks {
           return;
 
         const trace = result.attachments.find(a => a.name === 'trace')?.path || '';
+        // if trace viewer is currently displaying the trace file about to be replaced, it needs to be refreshed
+        const prevTrace = (testItem as any)[traceUrlSymbol];
         (testItem as any)[traceUrlSymbol] = trace;
-        if (enqueuedSingleTest)
+        if (enqueuedSingleTest || prevTrace === this._traceViewer.currentFile())
           this._showTrace(testItem);
 
         if (result.status === test.expectedStatus) {
